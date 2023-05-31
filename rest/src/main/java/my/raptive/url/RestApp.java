31ff85de4463449bd0f86bde0b864f56ad01d605
Wesley Ren <wesleyren@yahoo.com>
2023-05-30 20:12:11 -0700

    add h2 in server mode

--- rest/src/main/java/my/raptive/url/RestApp.java
+++ rest/src/main/java/my/raptive/url/RestApp.java
@@ -2,7 +2,11 @@ package my.raptive.url;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.h2.tools.Server;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 @EnableAsync
@@ -10,6 +14,12 @@ public class RestApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApp.class, args);
+	}
+
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server inMemoryH2DatabaseServer() throws SQLException {
+		return Server.createTcpServer(
+				"-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
 	}
 
 }
